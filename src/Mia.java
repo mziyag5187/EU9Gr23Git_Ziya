8f93e1279f3ad4ce5253a1f8f4f891bd5859b878
TheBullFinch <105283141+TheBullFinch@users.noreply.github.com>
2022-08-10 21:34:38 +0100

    Second change

--- src/Mia.java
+++ src/Mia.java
@@ -1,5 +1,6 @@
 public class Mia {
     public static void main(String[] args) {
         System.out.println("First change from Mia");
+        System.out.println("Second change from Mia");
     }
 }
