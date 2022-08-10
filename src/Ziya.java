c33d387a614ee8047540382427ed6fa5d0588feb
mziya <mziyag5187@gmail.com>
2022-08-10 22:55:31 +0300

    second commit from Ziya

--- src/Ziya.java
+++ src/Ziya.java
@@ -1,5 +1,6 @@
 public class Ziya {
     public static void main(String[] args) {
         System.out.println("First change from Ziya");
+        System.out.println("Second change from Ziya");
     }
 }
