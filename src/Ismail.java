08622bfc32f2863f6019c6a29844f0f9aaba821d
ismog <ismograpy@gmail.com>
2022-08-10 21:35:00 +0100

    second change from Ismail

--- src/Ismail.java
+++ src/Ismail.java
@@ -1,5 +1,6 @@
 public class Ismail {
     public static void main(String[] args) {
         System.out.println("First change from ismail");
+        System.out.println("First change from ismail");
     }
 }
